b0f4e6aa1786e60c96bf7ba8cebd37d60b78043e
AliElmallah <alielmallah22@gmail.com>
2021-06-25 14:10:45 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,78 +6,83 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+  chick:Boolean=false;
+  prouductArray:any[] = [];
+   formGrop1= new FormGroup({
+     productName:new FormControl(null, [Validators.required]),
+     productcatigory:new FormControl(null,[Validators.required]),
+     productPrice:new FormControl(null),
+   });
+
 constructor(){
+
+}
+ngOnInit(): void {
   if(localStorage.getItem("products") != null){
     this.prouductArray= JSON.parse(localStorage.getItem('products')  || '{}');
   }else{
 
     this.prouductArray=[];
-    
+
   }
 
 }
 
-  chick:Boolean=false;
- prouductArray:any[];
-  formGrop1= new FormGroup({
-    productName:new FormControl(null),
-    productcatigory:new FormControl(null),
-    productPrice:new FormControl(null),
-  });
+initForm(){
+  return {
+    productName: '',
+    productPrice:'',
+    productcatigory:''
+  }
+}
+
+resetForm(){
+  this.formGrop1.reset(this.initForm())
+}
+
   addProuduct(data:any){
 
-    this.prouductArray.push(data.value);
-    localStorage.setItem("products",JSON.stringify(this.prouductArray));
+    if(this.formGrop1.valid){
+
+      this.prouductArray.push(data.value);
+      localStorage.setItem("products",JSON.stringify(this.prouductArray));
+  this.resetForm()
+    }
+
+
   }
 
   deletProduct(x:number){
     this.prouductArray.splice(x , 1);
     localStorage.setItem("products",JSON.stringify(this.prouductArray));
   }
-  x:string='';
-  y:string='';
-  z:string='';
+
   display:boolean=false;
   currindex:number= -1;
   updatProuduct(index:number){
     this.display=true;
-    console.log(this.prouductArray[index]);
-    console.log(index);
-    this.x = this.prouductArray[index].productPrice;
-    this.y = this.prouductArray[index].productName;
-    this.z = this.prouductArray[index].productcatigory;
+    this.formGrop1.patchValue( this.prouductArray[index])
     this.currindex=index;
+
+
   }
 
   cancelproduct(){
-    this.x = '';
-    this.y = '';
-    this.z = '';
+this.resetForm()
     this.display=false;
   }
 
   okprouduct(form:any){
-    if(form.value.productName == null){
-      form.value.productName=this.y;
-    }
-    if(form.value.productPrice == null){
-      form.value.productPrice=this.x;
-    }
 
-    if(form.value.productcatigory == null){
-      form.value.productcatigory=this.z;
-    }
-    
-    this.prouductArray[this.currindex]=form.value;
-    console.log(this.prouductArray);
-    console.log(this.currindex);
-    let h = this.currindex;
-    
-    this.prouductArray.splice(h,1)
-    
-    
-    
-    
+
+    this.prouductArray.splice(this.currindex,1,form.value)
+    this.resetForm()
+
+
+
+
+
+
   }
 }
